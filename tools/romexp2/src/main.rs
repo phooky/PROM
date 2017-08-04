9219c3d7ae749de1052594e765df0736c4949018
Adam Mayer <phooky@gmail.com>
2017-08-04 17:10:02 -0400

    adding cli parsing

--- tools/romexp2/src/main.rs
+++ tools/romexp2/src/main.rs
@@ -1,10 +1,21 @@
 extern crate gtk;
+extern crate clap;
+
+use clap::{Arg,App};
 
 use gtk::prelude::*;
-
 use gtk::{Button, Window, WindowType};
 
 fn main() {
+    let matches = App::new("ROM image explorer")
+        .version("0.1")
+        .author("phooky@gmail.com")
+        .about("Quickly analyze ROM dumps and other binary blobs")
+        .arg(Arg::with_name("ROM")
+            .help("ROM file to analyze")
+            .required(true))
+        .get_matches();
+
     if gtk::init().is_err() {
         println!("Failed to initialize GTK.");
         return;
