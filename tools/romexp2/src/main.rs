3b70a89caaab329ef6ad4e209024830151922afd
Adam Mayer <phooky@gmail.com>
2017-08-07 22:33:07 -0400

    starting in on code reorg

--- tools/romexp2/src/main.rs
+++ tools/romexp2/src/main.rs
@@ -87,6 +87,89 @@ fn link_program(vs: GLuint, fs: GLuint) -> GLuint {
     }
 }
 
+struct Visualizer {
+    glfw : glfw::Glfw,
+    win : glfw::Window,
+    events : std::sync::mpsc::Receiver<(f64, glfw::WindowEvent)>,
+    program : GLuint,
+}
+
+impl Visualizer {
+    fn new(size : (u32, u32), data_sz : usize) -> Visualizer {
+        let mut glfw = glfw::init(glfw::FAIL_ON_ERRORS).unwrap();
+        let (mut window, events) = glfw.create_window(size.0, size.1 ,
+                                                      "ROM explorer",
+                                                      glfw::WindowMode::Windowed)
+            .expect("Failed to create GLFW window.");
+        window.set_key_polling(true);
+        window.make_current();
+        gl::load_with(|name| window.get_proc_address(name) as *const _);
+        // Create vertex shader
+        let vs = compile_shader(VS_SRC, gl::VERTEX_SHADER);
+        // Fragment shader needs size of data at compile time (in uints)
+        let fsstr = String::from(FS_SRC).replace("{}",(data_sz/4).to_string().as_str());
+        let fs = compile_shader(fsstr.as_str(), gl::FRAGMENT_SHADER);
+        let program = link_program(vs, fs);
+
+        let mut vao = 0; let mut vbo = 0;
+        unsafe {
+        // Create Vertex Array Object
+            gl::GenVertexArrays(1, &mut vao);
+            gl::BindVertexArray(vao);
+            // Create a Vertex Buffer Object and copy the vertex data to it
+            gl::GenBuffers(1, &mut vbo);
+            gl::BindBuffer(gl::ARRAY_BUFFER, vbo);
+            gl::BufferData(gl::ARRAY_BUFFER,
+                           (VERTEX_DATA.len() * mem::size_of::<GLfloat>()) as GLsizeiptr,
+                           mem::transmute(&VERTEX_DATA[0]),
+                           gl::STATIC_DRAW);
+            // Use shader program
+            gl::UseProgram(program);
+            gl::BindFragDataLocation(program, 0, CString::new("out_color").unwrap().as_ptr());
+            // Specify the layout of the vertex data
+            let pos_attr = gl::GetAttribLocation(program, CString::new("position").unwrap().as_ptr());
+            gl::EnableVertexAttribArray(pos_attr as GLuint);
+            gl::VertexAttribPointer(pos_attr as GLuint,
+                                    2,
+                                    gl::FLOAT,
+                                    gl::FALSE as GLboolean,
+                                    0,
+                                    ptr::null());
+        }
+        let vz = Visualizer { glfw : glfw, win : window, events: events, program : program };
+        vz.set_size(size);
+        vz
+    }
+
+    fn set_data(&self, dat : *const GLuint, len : usize) {
+        unsafe { 
+            let texloc = gl::GetUniformLocation(self.program,CString::new("rom").unwrap().as_ptr());
+            gl::Uniform1uiv(texloc,(len/4) as i32,dat);
+        }
+    }
+
+    fn set_size(&self, size : (u32, u32)) {
+        unsafe {
+            gl::Uniform1ui(
+                gl::GetUniformLocation(self.program,CString::new("ww").unwrap().as_ptr()),
+                size.0);
+            gl::Uniform1ui(
+                gl::GetUniformLocation(self.program,CString::new("wh").unwrap().as_ptr()),
+                size.1);
+        }
+    }
+
+    fn set_stride(&self, stride : u32) {
+        unsafe {
+            gl::Uniform1ui(
+                gl::GetUniformLocation(self.program,CString::new("stride").unwrap().as_ptr()),
+                stride);
+        }
+    }
+}
+
+        
+
 fn main() {
     let matches = App::new("ROM image explorer")
         .version("0.1")
@@ -104,66 +187,22 @@ fn main() {
     };
     
     println!("Opened {}; size {} bytes",rom_path,rom.len());
-    unsafe{println!("First byte: {:x}",rom.as_slice()[1]);};
-    let mut glfw = glfw::init(glfw::FAIL_ON_ERRORS).unwrap();
-    let (mut window, events) = glfw.create_window(512,512,"ROM explorer", glfw::WindowMode::Windowed)
-        .expect("Failed to create GLFW window.");
-    window.set_key_polling(true);
-    window.make_current();
-    gl::load_with(|name| window.get_proc_address(name) as *const _);
 
-    // Create GLSL shaders
-    let vs = compile_shader(VS_SRC, gl::VERTEX_SHADER);
-    let fsstr = String::from(FS_SRC).replace("{}",(rom.len()/4).to_string().as_str());
-    println!("{}",fsstr);
-    let fs = compile_shader(fsstr.as_str(), gl::FRAGMENT_SHADER);
-    let program = link_program(vs, fs);
-
-    let mut vao = 0; let mut vbo = 0;
-    unsafe {
-        // Create Vertex Array Object
-        gl::GenVertexArrays(1, &mut vao);
-        gl::BindVertexArray(vao);
-
-        // Create a Vertex Buffer Object and copy the vertex data to it
-        gl::GenBuffers(1, &mut vbo);
-        gl::BindBuffer(gl::ARRAY_BUFFER, vbo);
-        gl::BufferData(gl::ARRAY_BUFFER,
-                       (VERTEX_DATA.len() * mem::size_of::<GLfloat>()) as GLsizeiptr,
-                       mem::transmute(&VERTEX_DATA[0]),
-                       gl::STATIC_DRAW);
-        // Use shader program
-        gl::UseProgram(program);
-        gl::BindFragDataLocation(program, 0, CString::new("out_color").unwrap().as_ptr());
-        gl::Uniform1ui(gl::GetUniformLocation(program,CString::new("ww").unwrap().as_ptr()),512);
-        gl::Uniform1ui(gl::GetUniformLocation(program,CString::new("wh").unwrap().as_ptr()),512);
-        gl::Uniform1ui(gl::GetUniformLocation(program,CString::new("stride").unwrap().as_ptr()),8);
-        // Specify the layout of the vertex data
-        let pos_attr = gl::GetAttribLocation(program, CString::new("position").unwrap().as_ptr());
-        gl::EnableVertexAttribArray(pos_attr as GLuint);
-        gl::VertexAttribPointer(pos_attr as GLuint,
-                                2,
-                                gl::FLOAT,
-                                gl::FALSE as GLboolean,
-                                0,
-                                ptr::null());
-
-
-        let texloc = gl::GetUniformLocation(program,CString::new("rom").unwrap().as_ptr());
-        gl::Uniform1uiv(texloc,(rom.len()/4) as i32,rom.ptr() as *const GLuint);
-
-    }
-    while !window.should_close() {
+    let mut viz = Visualizer::new((512, 512), rom.len());
+    viz.set_data(rom.ptr() as *const GLuint, rom.len());
+    viz.set_stride(8);
+    
+    while !viz.win.should_close() {
         unsafe { gl::ClearColor(1.0,0.0,0.0,1.0) };
         unsafe { gl::Clear(gl::COLOR_BUFFER_BIT) };
         unsafe { gl::DrawArrays(gl::TRIANGLES, 0, 6) };
-        window.swap_buffers();
-        glfw.poll_events();
-        for (_, event) in glfw::flush_messages(&events) {
+        viz.win.swap_buffers();
+        viz.glfw.poll_events();
+        for (_, event) in glfw::flush_messages(&viz.events) {
             println!("EVT: {:?}", event);
             match event {
                 glfw::WindowEvent::Key(Key::Escape, _, Action::Press, _) => {
-                    window.set_should_close(true)
+                    viz.win.set_should_close(true)
                 },
                 _ => {},
             }
