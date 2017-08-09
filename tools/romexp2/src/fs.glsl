8d23512bdb66b7089332dd0f08300d4c202a5245
Adam Mayer <phooky@gmail.com>
2017-08-09 17:09:52 -0400

    minor cleanup

--- tools/romexp2/src/fs.glsl
+++ tools/romexp2/src/fs.glsl
@@ -10,7 +10,6 @@ uniform usampler1D romtex;
 void main() {
      uint x = uint(gl_FragCoord[0] - 0.5);
      uint y = (wh - 1u) - uint(gl_FragCoord[1] - 0.5);
-     uint rlen = uint({});
      uint col = x / stride;
      uint bitidx = ((y  + (col*wh)) * stride) + (x % stride);
      
@@ -18,12 +17,9 @@ void main() {
      uint tex_bit_off = bitidx % 8u;
 
      if (tex_off >= {}u) {
-     	out_color = vec4(0.0,0.0,0.8,1.0);
+     	out_color = vec4(0.0,0.0,0.4,1.0);
      	return;
 	}	
-     uint rv1 = (texture(romtex, (float(tex_off)+0.5)/{}.0).r >> (7u-tex_bit_off)) & 1u;
-     uint rv2 = 0u;
-     uint rv3 = 0u;
-     out_color = vec4(float(rv1)/1.0,float(rv2)/1.0,float(rv3)/1.0, 1.0);
-
+     uint rv = (texture(romtex, (float(tex_off)+0.5)/{}.0).r >> (7u-tex_bit_off)) & 1u;
+     out_color = vec4(float(rv),float(rv),float(rv), 1.0);
 }
